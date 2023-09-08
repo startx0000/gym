f9222746b708ee0c45ac6c211a353b0c56685d92
tedik <startx0000@github.com>
2023-09-08 20:29:56 +0200

    added servicess

--- movie/lib/widget/cardWorkout.dart
+++ movie/lib/widget/cardWorkout.dart
@@ -10,30 +10,33 @@ class CardWorkout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(blurRadius: 7,offset: const Offset(0, 7) ,color: Colors.grey[300]!)
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(blurRadius: 7,offset: const Offset(0, 7) ,color: Colors.grey[300]!)
 
-          ]
-      ),
-      margin: EdgeInsets.all(3.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
+            ]
+        ),
+        margin: EdgeInsets.all(3.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
 
-          SizedBox(height: 1.0,),
-          Text(' ${workout.description}',
-            style:   const TextStyle(
-                fontWeight: FontWeight.bold,
-                // fontSize: 12.0.sp,
-                color: Colors.grey
+            SizedBox(height: 1.0,),
+            Text(' ${workout.description}',
+              style:   const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  // fontSize: 12.0.sp,
+                  color: Colors.grey
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
+
       ),
-      
     );
   }
 }
